eec579886d244499b150ed87c336629ac9d963ca
Shubham Pal <115461478+Shubham0126@users.noreply.github.com>
2023-05-08 19:08:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 $(window).scroll(function(){
     if ($(this).scrollTop() > 100) {
         $('.back-to-top').fadeIn();
-        $('.navbar').addClass('bg-dark')
+        
     } else {
         $('#back-to-top').fadeOut();
-        $('.navbar').removeClass('bg-dark')
+        
     }
   });
   
@@ -14,6 +14,38 @@ $(window).scroll(function(){
   
     $('html, body').animate({scrollTop : 0},600);
     return false;
+  });
+
+  var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 3,
+      slideShadows: true
+    },
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2
+      },
+      768: {
+        slidesPerView: 1
+      },
+      1024: {
+        slidesPerView: 2
+      },
+      1560: {
+        slidesPerView: 3
+      }
+    }
   });
 
 //   var delay = 2000;
